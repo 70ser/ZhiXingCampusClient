eac57b6ffa52631eb9c796263bcec0e3ef535716
RB <biasedclover@gmail.com>
2023-05-06 17:26:19 +0800

    Add profile

--- Demo/app/src/main/java/com/zhixing/app/main/BlankFragment3.java
+++ Demo/app/src/main/java/com/zhixing/app/main/BlankFragment3.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.JsonObject;
 import com.zhixing.app.R;
+import com.zhixing.app.bean.Shop;
+import com.zhixing.app.common.HttpUtils;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +64,8 @@ public class BlankFragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.detail1, container, false);
     }
 }
